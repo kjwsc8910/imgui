a6dfd1016bb7e897223a3806b7c43cd076988e25
kjwsc8910 <05kj@sky.com>
2025-04-28 18:05:15 +0100

    Create Premake5.lua

---
+++ Premake5.lua
@@ -0,0 +1,25 @@
+project "ImGui"
+  kind "StaticLib"
+  language "C++"
+
+  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+  objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+  files {
+    "imconfig.h",
+    "imgui.cpp",
+    "imgui_draw.cpp",
+    "imgui_internal.h",
+    "imgui_windgets.cpp",
+    "imgui_rectpack.h",
+    "imgui_truetype.h",
+    "imgui_demo.cpp"
+  }
+
+  filter "system:windows"
+    systemversion "latest"
+    cppdialect "C++17"
+    staticruntime "On"
+
+  filter {"system:windows", "configurations:Release" }
+    buildoptions "/MT"
